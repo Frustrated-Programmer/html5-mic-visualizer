407b62ecc8cfeae860b904b4612d9f18a4d0c976
FrustratedProgrammer <eliassantiagoanderson@gmail.com>
2020-08-29 18:41:20 -0500

    Added more visualizations.

--- js/index.js
+++ js/index.js
@@ -1,19 +1,30 @@
 let mic = new Mic();
-var h = document.getElementsByTagName("h1")[0];
-var hSub = document.getElementsByTagName("h1")[1];
+let h = document.getElementById("header");
+let canvasANDshape = document.getElementById('canvasANDshape');
+let canvasANDshapeCTX = canvasANDshape.getContext("2d");
+let canvasANDimage = document.getElementById('canvasANDimage');
+let canvasANDimageCTX = canvasANDimage.getContext("2d");
+let canvasANDvideo = document.getElementById('canvasANDvideo');
+let canvasANDvideoCTX = canvasANDvideo.getContext("2d");
+let hiddenVideo = document.getElementById('hiddenVideo');
+let image = document.getElementById('image');
 let seconds = 0;
+let img = new Image();
+img.src = "./circle.png";
+image.style.height = "0px";
+image.style.display = "";
+image.style.width = "100px";
 mic.onDBChange = function(DB){
     h.innerHTML = Math.round(DB) + " dB";
-    if (DB >= 30) {
-        seconds += 1/mic.updateTime;
-        if (seconds >= 5) {
-            hSub.innerHTML = "You’ve been in loud environment for<span> " + Math.floor(seconds) + " </span>seconds." ;
-        }
-    }
-    else {
-        seconds = 0;
-        hSub.innerHTML = "";
-    }
+    canvasANDshapeCTX.clearRect(0, 0, canvasANDshape.width, canvasANDshape.height);
+    canvasANDimageCTX.clearRect(0, 0, canvasANDimage.width, canvasANDimage.height);
+    canvasANDshapeCTX.fillRect(50,100-(DB*0.5),100,DB);
+    canvasANDimageCTX.drawImage(img,50,100-(DB*2),100,(DB*4));
+    image.style.height = DB*3+"px";
+    //MAX = 2.88 | MIN = 0;
+    //DB = 40    | DB = 0;
+    hiddenVideo.currentTime = 2.88 - (DB / 13.888);
+    canvasANDvideoCTX.drawImage(hiddenVideo,0,0,200,200);
 };
 mic.onFrequency = function(frequencyArray){
     let adjustedLength;
